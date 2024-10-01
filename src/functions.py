b6bde3d526686894144d658a962ee0fb612c49f6
Erik <erikjro@uio.no>
2024-10-01 18:19:26 +0200

    updated functions.py

--- src/functions.py
+++ src/functions.py
@@ -10,10 +10,6 @@ from sklearn.preprocessing import StandardScaler, PolynomialFeatures
 from sklearn.metrics import mean_squared_error
 from sklearn.pipeline import make_pipeline
 from sklearn.utils import resample
-plt.rcParams.update({'text.usetex': True, 'font.size': 16, 'font.family': 'serif', 'font.serif': 'Computer Modern Sans Serif', 'font.weight': 100, 'mathtext.fontset': 'cm', 'xtick.labelsize': 14, 'ytick.labelsize': 14})
-
-
-
 
 
 
@@ -49,7 +45,7 @@ def create_X(x, y, n):
 		y = np.ravel(y)
 
 	N = len(x)
-	l = int((n+1)*(n+2)/2)		# Number of elements in beta
+	l = int((n+1)*(n+2)/2) # Number of elements in beta
 	X = np.ones((N,l))
 
 	for i in range(1,n+1):
@@ -59,7 +55,112 @@ def create_X(x, y, n):
 
 	return X
 
-def kfold_crossval(x, y, z, k, model, degrees):
+
+def OLS(x, y, z, degrees, scale=True, test_size=0.2, seed=None, intercept=False):
+    """
+    Performs OLS regression. Returns MSE and R2 score and beta values. 
+    """
+
+    MSE_list = []
+    R2_list = []
+    β_list = []
+
+    for degree in degrees:
+        X = create_X(x, y, degree)
+        X_train, X_test, z_train, z_test = train_test_split(X, z, test_size=test_size, random_state=seed)
+
+        if scale: # Scaling the data
+            scaler_X = StandardScaler().fit(X_train)
+            X_train = scaler_X.transform(X_train)
+            X_test = scaler_X.transform(X_test)
+
+            scaler_z = StandardScaler().fit(z_train)
+            z_train = scaler_z.transform(z_train)
+            z_test= scaler_z.transform(z_test)
+
+
+        β = np.linalg.pinv(X_train.T @ X_train) @ X_train.T @ z_train
+
+        if intercept:
+            β[0] = 0
+
+        β_list.append(β)
+        
+        z_tilde = X_train @ β 
+        z_pred = X_test @ β
+        
+        MSE_list.append(MSE(z_train, z_tilde))
+        R2_list.append(R2(z_test, z_pred))
+
+    return MSE_list, R2_list, β_list
+
+
+def Ridge(x, y, z, degrees, λ, scale=True, test_size=0.2, seed=None, intercept=False):
+    """Performs Ridge regression. Returns MSE and R2 score and beta values"""
+    MSE_list = np.zeros(len(degrees))
+    R2_list = np.zeros(len(degrees))
+    β_list = []
+
+    for i, degree in enumerate(degrees):
+        X = create_X(x, y, degree)
+        X_train, X_test, z_train, z_test = train_test_split(X, z, test_size=test_size, random_state=seed)
+
+        if scale: # Scaling the data
+            scaler_X = StandardScaler().fit(X_train)
+            X_train = scaler_X.transform(X_train)
+            X_test = scaler_X.transform(X_test)
+
+            scaler_z = StandardScaler().fit(z_train)
+            z_train = scaler_z.transform(z_train)
+            z_test= scaler_z.transform(z_test)
+
+        β = np.linalg.pinv(X_train.T @ X_train + λ*np.eye(X_train.shape[1])) @ X_train.T @ z_train
+
+        if intercept:
+            β[0] = 0
+
+        β_list.append(β)
+
+        z_tilde = X_train @ β
+        z_pred = X_test @ β
+
+        MSE_list[i] = MSE(z_train, z_tilde)
+        R2_list[i] = R2(z_test, z_pred)
+
+def Lasso(x, y, z, degrees, λ, scale=True, test_size=0.2, seed=None, intercept=False):
+    """Performs Lasso regression. Returns MSE and R2 score and beta values"""
+    MSE_list = np.zeros(len(degrees))
+    R2_list = np.zeros(len(degrees))
+    β_list = []
+
+    for i, degree in enumerate(degrees):
+        X = create_X(x, y, degree)
+        X_train, X_test, z_train, z_test = train_test_split(X, z, test_size=test_size, random_state=seed)
+
+        if scale: # Scaling the data
+            scaler_X = StandardScaler().fit(X_train)
+            X_train = scaler_X.transform(X_train)
+            X_test = scaler_X.transform(X_test)
+
+            scaler_z = StandardScaler().fit(z_train)
+            z_train = scaler_z.transform(z_train)
+            z_test= scaler_z.transform(z_test)
+
+        RegLasso = linear_model.Lasso(λ, fit_intercept=True)
+        RegLasso.fit(X_train, z_train)
+
+        z_tilde = RegLasso.predict(X_train)
+        z_pred = RegLasso.predict(X_test)
+
+        MSE_list[i] = MSE(z_train, z_tilde)
+        R2_list[i] = R2(z_test, z_pred)
+
+
+    # print(MSE_list.shape, R2_list.shape, β.shape)
+
+    return MSE_list, R2_list, β_list
+
+def Kfold_crossval(x, y, z, k, model, degrees):
     """
     Performs k-fold cross-validation.
     """
@@ -81,22 +182,40 @@ if __name__ == "__main__":
 
     x = np.arange(0, 1, 1/N)
     y = np.arange(0, 1, 1/N)
+    X, Y = np.meshgrid(x, y)
 
-    z = FrankeFunction(x, y)
+    z = FrankeFunction(X, Y)
 
+    """"Task a"""
+    degrees = np.arange(1, 6)
+    MSE_list, R2_list, β_list = OLS(x, y, z, degrees, seed=seed)
+
+    """Task b"""
+    degrees = np.arange(1, 6)
+    lambdas = np.logspace(-4, 4, 6)
+
+    MSE_list = np.zeros((len(lambdas), len(degrees)))
+    R2_list = np.zeros((len(lambdas), len(degrees)))
+    β_list = [[] for i in range(len(lambdas))]
+
+
+    for i, lmb in enumerate(lambdas):
+        MSE_list[i], R2_list[i], β_list[i] = Ridge(x, y, z, degrees, lmb, seed=seed)
+
+    """Task f"""
     degrees = np.arange(1, 6) 
     k = 5
 
-    """kfold OLS"""    
+    # kfold OLS  
     OLS = LinearRegression(fit_intercept=False)
-    OLS_mse_kfold = kfold_crossval(x, y, z, k, OLS, degrees)
+    OLS_mse_kfold = Kfold_crossval(x, y, z, k, OLS, degrees)
   
-    """kfold Ridge"""
+    # kfold Ridge
     lambdas = np.logspace(-4, 4, 6)
     ridge_mse_kfold = np.zeros((len(lambdas), len(degrees)))
 
     for i, lmb in enumerate(lambdas):
         ridge = linear_model.Ridge(alpha=lmb, fit_intercept=False)
-        ridge_mse_kfold[i] = kfold_crossval(x, y, z, k, ridge, degrees)
+        ridge_mse_kfold[i] = Kfold_crossval(x, y, z, k, ridge, degrees)
 
-    print(OLS_mse_kfold.shape, ridge_mse_kfold.shape)
+    # print(OLS_mse_kfold.shape, ridge_mse_kfold.shape)
